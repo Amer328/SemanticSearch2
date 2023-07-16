f64abc45b3b694931bd596f9ab5bf4084efeb973
Amer328 <67227693+Amer328@users.noreply.github.com>
2023-07-16 19:31:11 +0100

    Update qa.py

--- qa.py
+++ qa.py
@@ -17,7 +17,7 @@ def create_prompt(context,query):
 
 def generate_answer(prompt):
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model="gpt-3.5-turbo-16k",
     messages=[{"role": "user", "content": prompt}],
     temperature=1,
     max_tokens=2000,
