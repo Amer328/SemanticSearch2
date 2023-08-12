cda91e24e2c4c7ac6049cf3f835d877df034ec60
Amer328 <67227693+Amer328@users.noreply.github.com>
2023-08-12 13:43:48 +0000

    Update vector_search.py

--- vector_search.py
+++ vector_search.py
@@ -6,7 +6,7 @@ model = SentenceTransformer('all-MiniLM-L6-v2') #384 dimensional
 
 pinecone_api_key = st.secrets["PINECONE_API"]
 
-pinecone.init(api_key=pinecone_api_key, environment="us-east-1-aws") 
+pinecone.init(api_key=pinecone_api_key, environment="gcp-starter") 
 index = pinecone.Index("generalpurpose1")
 
 
